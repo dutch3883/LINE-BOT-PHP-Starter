7d316d003aee8b64ee047d7b9ccf8e5e13b5f5ec
dutch3883 <panuwach.boo@student.mahidol.ac.th>
2017-08-09 15:04:49 +0700

    l

--- bot.php
+++ bot.php
@@ -63,7 +63,7 @@ if (!is_null($events['events'])) {
 						]
 					]
 				]
-			]
+			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
@@ -73,19 +73,20 @@ if (!is_null($events['events'])) {
 				'messages' => [$messages],
 			];
 
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			// $post = json_encode($data);
+			// $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			// $ch = curl_init($url);
+			// curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			// curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			// curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			// curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			
-			#if($userId == "U2ac1c54ff5ae78e18042a8c135a2bea9")	
-			if($text== "show") $result = curl_exec($ch);
-			curl_close($ch);
+			//if($userId == "U2ac1c54ff5ae78e18042a8c135a2bea9")	
+			echo $data;
+			// if($text== "show") $result = curl_exec($ch);
+			// curl_close($ch);
 
 			echo $result . "\r\n";
 		}
