922b9abf0365c2e9952c54fc720c3de0f692f0f9
dutch3883 <panuwach.boo@student.mahidol.ac.th>
2017-08-09 15:11:32 +0700

    l

--- bot.php
+++ bot.php
@@ -73,20 +73,20 @@ if (!is_null($events['events'])) {
 				'messages' => [$messages],
 			];
 
-			// $post = json_encode($data);
-			// $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			// $ch = curl_init($url);
-			// curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			// curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			// curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			// curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 			
-			//if($userId == "U2ac1c54ff5ae78e18042a8c135a2bea9")	
-			echo json_encode($data);
-			// if($text== "show") $result = curl_exec($ch);
-			// curl_close($ch);
+			if($userId == "U2ac1c54ff5ae78e18042a8c135a2bea9")	
+
+			$result = curl_exec($ch);
+			curl_close($ch);
 
 			echo $result . "\r\n";
 		}
