47a7471bc51edf0f3d41ce729067f14a4738ae6c
dutch3883 <panuwach.boo@student.mahidol.ac.th>
2017-08-09 15:53:32 +0700

    k

--- bot.php
+++ bot.php
@@ -27,7 +27,7 @@ if (!is_null($events['events'])) {
 				'type' => 	'template',
 				'altText' =>  'alternative text',
 				'template' => [
-					'type'	=> 'carousel',
+					'type'	=> 'cr',
 					'columns' => [
 						[
 							'thumbnailImageUrl' => 'https://www.google.co.th/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
