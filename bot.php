fdf14dd0891a88446a99b21f03720a8cddde355d
dutch3883 <panuwach.boo@student.mahidol.ac.th>
2017-08-09 16:22:07 +0700

    ll

--- bot.php
+++ bot.php
@@ -73,7 +73,7 @@ if (!is_null($events['events'])) {
 			// ];
 
 
-			$messages [
+			$messages = [
 				'type' => 'template',
 				'altText' => 'this is aconfirm template',
 				'template' => [
@@ -91,7 +91,7 @@ if (!is_null($events['events'])) {
 						]
 					]
 				]
-			]
+			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
